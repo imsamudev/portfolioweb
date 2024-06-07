f9095aee2ac013b413774c5e3e440a3d10613de2
SamuDev <samudev01@gmail.com>
2024-06-07 09:48:54 -0300

    return button functionality added

--- main.js
+++ main.js
@@ -4,3 +4,32 @@ const navbarLinks = document.getElementById("navbarLinks");
 navbarToggle.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
+
+// returnBtn to nav function
+var btn = document.querySelector(".returnBtn");
+
+window.onload = function () {
+  btn.style.display = "none";
+};
+
+window.onscroll = function () {
+  var navbar = document.querySelector(".navbar").offsetHeight;
+  if (window.pageYOffset > navbar) {
+    btn.style.display = "block";
+    btn.style.transition = "opacity 0.5s";
+    btn.style.opacity = "1";
+  } else {
+    btn.style.opacity = "0";
+    setTimeout(function () {
+      if (window.pageYOffset <= navbar) {
+        btn.style.display = "none";
+      }
+    }, 500);
+  }
+};
+btn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
